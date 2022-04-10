a750ba0337899afe2f410a3bed94d0a4f29aafc7
Ain Chung <artnouveau0804@gmail.com>
2022-04-10 17:54:22 +0900

    (1) - Two Sum (오타고침)
    for문에 j는 i와 같으면 안되기 때문에 i+1를 해준다.

--- leetcode.js
+++ leetcode.js
@@ -3,7 +3,7 @@
 const twoSums = function (nums, target) {
   const n = nums.length;
   for (let i = 0; i < n - 1; i++) {
-    for (let j = 0; j < n; j++) {
+    for (let j = i + 1; j < n; j++) {
       if (nums[i] + nums[j] === target) {
         return [i, j];
       }
